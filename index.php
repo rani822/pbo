4468e18e695761398631d988c4a0648786141082
rani822 <72178721+rani822@users.noreply.github.com>
2021-05-15 09:31:35 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title> Form login user</title>
-</head>
-<body>
-    <h2> login user</h2>
-    <form action="login.php" method="post">
-        <div>
-            <label>Username</label>
-            <input type="text" name="username" id="username" />
-        </div>
-        <div>
-            <label>password</password>
-            <input type="password" name="password" id="password" />
-        </div>
-        <div>
-            <input type="submit" value="submit">
-    </form>
-</body>
-</html>
+<?php
+
+include '../koneksi.php';
+
+session_start();
+
+if($_SESSION['status'] != "login") {
+    header("location:../index.php");
+}
+
+echo "selamat datang"
+
+?>

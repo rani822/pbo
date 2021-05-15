f0075645118ac695ae9811b9e67a7d434384e631
rani822 <72178721+rani822@users.noreply.github.com>
2021-05-15 09:30:23 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Form login user</title>
+</head>
+<body>
+    <h2> login user</h2>
+    <form action="login.php" method="post">
+        <div>
+            <label>Username</label>
+            <input type="text" name="username" id="username" />
+        </div>
+        <div>
+            <label>password</password>
+            <input type="password" name="password" id="password" />
+        </div>
+        <div>
+            <input type="submit" value="submit">
+    </form>
+</body>
+</html>
